5b63d515d9564e5775b4aa113c19a9745d37bfb1
Hecate2 <2474101468@qq.com>
2023-07-10 17:53:50 +0800

    save progress on exception

--- polynetwork.py
+++ polynetwork.py
@@ -1,16 +1,34 @@
-from web3 import Web3
-import requests
 import json
+import pickle
 import queue
+# from web3 import Web3
+import requests
 
 # Connect to an Ethereum node
 url = 'https://eth-mainnet.g.alchemy.com/v2/_ki9mpf_eLMuhMmHxDTDm62gmS9yRZrD'
-w3 = Web3(Web3.HTTPProvider(url))
+# w3 = Web3(Web3.HTTPProvider(url))
 session = requests.Session()
 
-fetched_txs = set()
-fetched_addresses = set()
+try:
+    with open('fetched_txs.pkl', 'rb') as f:
+        fetched_txs = pickle.load(f)
+except:
+    fetched_txs = set()
+try:
+    with open('fetched_addresses.pkl', 'rb') as f:
+        fetched_addresses = pickle.load(f)
+except:
+    fetched_addresses = set()
 addresses_to_fetch = queue.Queue()
+try:
+    with open('addresses_to_fetch.txt', 'r') as f:
+        for line in f:
+            addresses_to_fetch.put(line.replace('\n', ''))
+except:
+    pass
+output = open('results.txt', 'a')
+
+
 
 def fetch_asset_transfers_from_address(address):
     # Alchemy API request payload
@@ -41,9 +59,9 @@ def fetch_asset_transfers_from_address(address):
             #     print(f"Asset Value: {transfer['value']}")
             return [t for t in txs if t['from'].lower() == address.lower()]
         else:
-            print("Error: No result found in the response.")
+            raise ValueError("Error: No result found in the response.")
     else:
-        print(f"Error: Request failed with status code {response.status_code}.")
+        raise ValueError(f"Error: Request failed with status code {response.status_code}.")
 
 
 def fetch_transactions(address):
@@ -58,10 +76,13 @@ def fetch_transactions(address):
         fetched_txs.add(tx['hash'])
         if int(tx['blockNum'], 16) >= 17660325:
             print('\r', end='')
-            print(f"[{tx['hash']}] {tx['value']} {tx['asset']} from {tx['from']} to {tx['to']}")
+            output_string = f"{tx['hash']} {tx['value']} {tx['asset']} from {tx['from']} to {tx['to']}\n"
+            print(output_string, end='')
+            output.write(output_string)
+            output.flush()
         else:
             print('\r', end='')
-            print(f"Omitting {tx['hash']} at block {int(tx['blockNum'], 16)}", end='')
+            print(f"{addresses_to_fetch.qsize()} addr in queue; Omitting {tx['hash']} at block {int(tx['blockNum'], 16)}", end='')
         if tx['to'] not in fetched_addresses:
             fetched_addresses.add(tx['to'])
             addresses_to_fetch.put(tx['to'])
@@ -71,5 +92,17 @@ def fetch_transactions(address):
 starting_address = '0xe0Afadad1d93704761c8550F21A53DE3468Ba599'
 # Fetch transactions recursively
 addresses_to_fetch.put(starting_address)
-while not addresses_to_fetch.empty():
-    fetch_transactions(addresses_to_fetch.get())
+
+try:
+    while not addresses_to_fetch.empty():
+        current_address = addresses_to_fetch.get()
+        fetch_transactions(current_address)
+except:
+    with open('fetched_txs.pkl', 'wb') as f:
+        pickle.dump(fetched_txs, f)
+    with open('fetched_addresses.pkl', 'wb') as f:
+        pickle.dump(fetched_addresses, f)
+    with open('addresses_to_fetch.txt', 'w') as f:
+        while not addresses_to_fetch.empty():
+            f.write(addresses_to_fetch.get() + '\n')
+        f.write(current_address + '\n')
